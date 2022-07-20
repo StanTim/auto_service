e180d8ad72dcd741f305b29394b1375d5d711dd2
StanTim <1694569@mail.ru>
2022-07-20 12:55:27 +0300

    add order_lists tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_07_20_074024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_lists", force: :cascade do |t|
+    t.bigint "service_id", null: false
+    t.bigint "specialist_id", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_order_lists_on_order_id"
+    t.index ["service_id"], name: "index_order_lists_on_service_id"
+    t.index ["specialist_id"], name: "index_order_lists_on_specialist_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "client_name"
+    t.string "phone"
+    t.string "car"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_services_on_category_id"
+  end
+
+  create_table "specialists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "order_lists", "orders"
+  add_foreign_key "order_lists", "services"
+  add_foreign_key "order_lists", "specialists"
+  add_foreign_key "services", "categories"
 end
