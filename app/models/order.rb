dc106787e368620a3638713e4f9bd4cd95dd76fc
StanTim <1694569@mail.ru>
2022-07-22 07:54:02 +0300

    accepts_nested_attributes_for

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,12 @@
 class Order < ApplicationRecord
   has_many :specialists, through: :order_lists
   has_many :services, through: :order_lists
-  has_many :order_lists
+  has_many :order_lists, dependent: :destroy
+  accepts_nested_attributes_for :order_lists, allow_destroy: true
+
 
   validates :client_name, presence: true,
-            length: { maximum: 50 },
-            format: { with: /\A[a-zA-Z]+\z/ }
+            length: { maximum: 50 }
 
   validates :phone, presence: true,
             :numericality => true,
