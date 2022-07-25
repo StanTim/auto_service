befd028113939cbe4c4947c60268b0a59912a4c1
StanTim <1694569@mail.ru>
2022-07-25 20:36:49 +0300

    order_list resurces

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
     resources :order_lists
   end
 
-  resources :specialists
   resources :services
   resources :categories
+  resources :specialists
+  resources :order_lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
