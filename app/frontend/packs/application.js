91607fc07a49a01aaa897100de92512d70dc83a9
StanTim <1694569@mail.ru>
2022-07-28 10:13:13 +0300

    fix deploy error

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,14 +3,6 @@ Rails.start()
 
 import "bootstrap/dist/js/bootstrap"
 
-import "../styles/application.scss"
-import "../styles/application.css"
-import "../styles/specialists.scss"
-import "../styles/order_lists.scss"
-import "../styles/orders.scss"
-import "../styles/services.scss"
-import "../styles/specialists.scss"
-
 import "./listener"
 
 const images = require.context("../images", true)
