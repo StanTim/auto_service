2f468c64a550fe54db69caa2fb0f80d967b54c25
StanTim <1694569@mail.ru>
2022-07-28 09:46:50 +0300

    comment styles

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -4,13 +4,13 @@ Rails.start()
 import "bootstrap/dist/js/bootstrap"
 
 import "../styles/application.scss"
-import "../styles/application.css"
-import "../styles/specialists.scss"
-import "../styles/order_lists.scss"
-import "../styles/orders.scss"
-import "../styles/services.scss"
-import "../styles/specialists.scss"
-
+// import "../styles/application.css"
+// import "../styles/specialists.scss"
+// import "../styles/order_lists.scss"
+// import "../styles/orders.scss"
+// import "../styles/services.scss"
+// import "../styles/specialists.scss"
+//
 import "./listener"
 
 import "bootstrap"
